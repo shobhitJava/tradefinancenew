68d93627ae504a849de7c2ca4180179c9ea7d923
shobhitJava <simplyshubh@gmail.com>
2017-09-05 13:31:26 +0530

    bol12

--- purchaseOrder.go
+++ purchaseOrder.go
@@ -227,7 +227,7 @@ func (t *PurchaseOrder) updatePOStatus(stub shim.ChaincodeStubInterface, args []
 		return nil, errors.New("Failed to unmarshal getAllRecordsList ")
 	}
 	po["Status"] = args[1]
-	po["Action"] = "Importer"
+	po["Action"] = "ImporterBank"
 	outputBytes, _ := json.Marshal(po)
 	stub.PutState(poNumber, outputBytes)
 
@@ -262,7 +262,7 @@ func (t *PurchaseOrder) updatePODetails(stub shim.ChaincodeStubInterface, args [
 		po["ExporterBank"] = args[1]
 		po["IsLCRequired"] = args[2]
 		po["Status"] = args[3]
-		po["Action"] = "ImporterBank"
+		po["Action"] = "Importer"
 		outputBytes, _ := json.Marshal(po)
 		stub.PutState(poNumber, outputBytes)
 	} else {
